8b57aaf0af95b9a77c8bc1f9c6563f7e90cc5f99
InterestsFantastic <30320765+InterestsFantastic@users.noreply.github.com>
2022-10-03 10:38:41 -0400

    Not sure if this is better but it works

--- mt2e_mechanics.py
+++ mt2e_mechanics.py
@@ -1,6 +1,24 @@
 #!/usr/bin/python3
 from rpgroller.roller import roll
 from random import randint
+from ODSReader.odsreader import ODSReader
+from ODSReader.utils import keyval
+
+mechanics_file = 'mt2e_mechanics.ods'
+mechanics = ODSReader(mechanics_file, clonespannedcolumns=True)
+
+characteristic_modifiers = mechanics.getSheet('CharacteristicModifiers')
+characteristic_modifiers = keyval(characteristic_modifiers, int)
+
+def characteristic_modifier(num):
+    '''Returns the diceroll modifier for having a particular value
+    in a characteristic'''
+    if num in characteristic_modifiers:
+        return characteristic_modifiers[num]
+    elif num > 14:
+        return 3
+    assert False, f'Improper characteristic modifier requested: {num}'
+
 
 def rd66():
     return randint(1,6) * 10 + randint(1,6)
@@ -21,30 +39,4 @@ def roll_boon():
 
 def roll_bane():
     return roll('3d6kl2')
-
-characteristic_modifiers = {
-    0: -3,
-    1: -2,
-    2: -2,
-    3: -1,
-    4: -1,
-    5: -1,
-    6: 0,
-    7: 0,
-    8: 0,
-    9: 1,
-    10: 1,
-    11: 1,
-    12: 2,
-    13: 2,
-    14: 2}
-
-def characteristic_modifier(num):
-    '''Returns the diceroll modifier for having a particular value
-    in a characteristic'''
-    if num in characteristic_modifiers:
-        return characteristic_modifiers[num]
-    elif num > 14:
-        return 3
-    assert False, f'Improper characteristic modifier requested: {num}'
 
