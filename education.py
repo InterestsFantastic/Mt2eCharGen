5ea41ca54cc57ffcfbc4d65f78a02df8377edaaa
InterestsFantastic <30320765+InterestsFantastic@users.noreply.github.com>
2022-10-03 14:53:34 -0400

    Issues graduating with honors

--- education.py
+++ education.py
@@ -11,15 +11,27 @@ class Education:
     def attempt_entry(self, char):
         # generate DMs
         dm = 0
-
         if char.terms > 3:
             return False
         elif char.terms == 3:
             dm += self.entry_dm_term3
         elif char.terms == 2:
             dm += self.entry_dm_term2
-    
         if char.soc >= 9:
             dm += self.entry_dm_soc9
 
         return char.characteristic_roll(self.entry, dm)
+
+    def attempt_grad(self, char):
+        # generate DMs
+        dm = 0
+        if char.end >= 8:
+            dm += grad_dm_end8
+        if char.soc >= 8:
+            dm += grad_dm_soc8
+
+        assert False, 'Need to figure out how to catch graduating with honors.'
+        # characteristic_target vs. characteristic_roll?
+        # change characteristic i.e. int +1 in character?
+        return char.characteristic_roll(self.grad, dm)
+        
