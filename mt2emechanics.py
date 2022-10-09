ce9c8aea41eaefefb7122e0504e3a3543a125591
InterestsFantastic <30320765+InterestsFantastic@users.noreply.github.com>
2022-10-09 15:04:03 -0400

    Steamlined variable creation and renamed funcs

--- mt2emechanics.py
+++ mt2emechanics.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 from ODSReader.odsreader import ODSReader
-from ODSReader.utils import dict_sheet_to_dict, rows_to_list_of_dicts, dict_of_dicts_from_list_of_dicts
+from ODSReader.utils import keyval_sheet_to_dict, rows_to_list_of_dicts, dict_of_dicts_from_list_of_dicts, dict_sheet_to_dict_of_dicts
 from education import create_educations
 
 mechanics_file = 'mt2emechanics.ods'
 mechanics = ODSReader(mechanics_file, clonespannedcolumns=True)
 
-characteristic_modifiers = mechanics.getSheet('CharacteristicModifiers')
-characteristic_modifiers = dict_sheet_to_dict(characteristic_modifiers, int)
+characteristic_modifiers = keyval_sheet_to_dict(mechanics, 'CharacteristicModifiers', int)
 def characteristic_modifier(num):
     '''Returns the diceroll modifier for having a particular value
     in a characteristic'''
@@ -19,8 +18,7 @@ def characteristic_modifier(num):
         # negative numbers.
         assert False, f'Improper characteristic modifier requested: {num}'
 
-noble_titles = mechanics.getSheet('NobleTitles')
-noble_titles = dict_sheet_to_dict(noble_titles, int, str)
+noble_titles = keyval_sheet_to_dict(mechanics, 'NobleTitles', int, str)
 def noble_title(num):
     '''Returns the noble title of a social status.'''
     if num in noble_titles:
@@ -33,9 +31,7 @@ educations_funcs = [str, str, int, int, int, str, int, int, int]
 educations = rows_to_list_of_dicts(education_sheet, *educations_funcs)
 educations = create_educations(educations)
 
-skills_sheet = mechanics.getSheet('Skills')
-skills = rows_to_list_of_dicts(skills_sheet)
-skills = dict_of_dicts_from_list_of_dicts('skill', skills)
+skills = dict_sheet_to_dict_of_dicts(mechanics, 'Skills', 'skill')
 skills_aliases = {}
 for skill in skills:
     if skills[skill]['short'] is not None:
@@ -49,3 +45,7 @@ def get_skill_name(skill):
         return skills_aliases[skill]
     else:
         assert False, f'Skill not found: {skill}.'
+
+print(educations[0])
+print(educations['University'])
+print(skills['Science (Philosophy)'])
