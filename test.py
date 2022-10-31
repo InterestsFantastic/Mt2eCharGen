0e7e1671bd9b2adc632ba6ec0b1814efcc799eb4
InterestsFantastic <30320765+InterestsFantastic@users.noreply.github.com>
2022-10-31 19:36:29 -0400

    Starting work on gaining ally

--- test.py
+++ test.py
@@ -1,21 +1,20 @@
 from mt2emechanics import events#, skills, skills_aliases
-from agent import Console
 from character import Character
 
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
 
-console = Console()
 c = Character()
 print(f'Character created with characteristics: {c.upp}\n\n')
+events['life'][3].run(c)
+input(c.log)
 
-##print(events['life'][2].script)
-events['life'][3].run(c, console)
+##for x in range(25):
+##    events['life'][2].run(c)
+##    print(c.characteristics_string)
+##pp.pprint(c.log)
+##print('\n\n\n')
+##input()
 
-for x in range(25):
-    events['life'][2].run(c, console)
-    print(c.characteristics_string)
-    
-pp.pprint(c.log)
-print('\n\n\n')
-
+events['life'][5].run(c)
+##pp.pprint(c.log)
