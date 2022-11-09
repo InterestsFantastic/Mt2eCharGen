f79c1e971084d556ae9e8cf35380ad7f25b897dd
InterestsFantastic <30320765+InterestsFantastic@users.noreply.github.com>
2022-11-09 14:25:25 -0500

    Tidying

--- test.py
+++ test.py
@@ -1,44 +1,16 @@
-from careers import events#, skills, skills_aliases
-from character import Character, parse_gain_skill
+from careers import events
+from character import Character
 from agent import Console
 
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
 
-##input('imports complete')
 c = Character()
 c.agent = Console
-
 print(c.char_description)
 
-##print(parse_gain_skill('carouse 0'))
-##print(parse_gain_skill('carouse 1'))
-##print(parse_gain_skill('carouse +1'))
-##print(parse_gain_skill('Animals (Handling) +1'))
-##print(parse_gain_skill('Animals (handling) +1'))
-##c.gain_skill('carouse 0')
-##c.gain_skill('carouse 1')
-##c.gain_skill('carouse +1')
-##c.gain_skill('carouse 0')
-##c.gain_skill('Animals (Handling) +1')
-##c.gain_skill('Animals (handling) +1')
-##c.gain_skill('riding +1')
-##c.gain_skill('vacc suit +1')
-##c.gain_skill('vacc +1')
-##print(c.skills)
-
-##print(parse_gain_skill('carouse -1')) # Works
 events['life'][3].run(c)
-
-##for x in range(25):
-##    events['life'][2].run(c)
-##    print(c.characteristics_string)
-##pp.pprint(c.log)
-##print('\n\n\n')
-##input()
-
 events['life'][2].run(c)
-events['life'][3].run(c)
 events['life'][5].run(c)
 events['life'][6].run(c)
 events['life'][7].run(c)
@@ -50,15 +22,8 @@ events['life'][4].run(c)
 events['life'][11].run(c)
 events['life'][8].run(c)
 events['edu'][2].run(c)
-##c.gain('ally 2')
-##c.gain('ally 2')
-##c.gain('ally 2')
-##c.gain('soc 12')
 
 pp.pprint(c.log)
 print(c.char_description)
 print(f'Benefit dms: {c.benefit_dms}')
 print(f'Next qual dm: {c.qual}')
-##c.gain('soc -2')
-##c.gain('ally -2')
-##print(c.char_description)
